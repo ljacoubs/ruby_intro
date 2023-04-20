5b81aac41bad68621d32cd380f13de812ebf5e99
Lou Jacoubs <lou.jacoubs@kellogg.northwestern.edu>
2023-04-20 01:41:30 +0000

    class work session 2

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -4,9 +4,35 @@
 
 # Arrays are lists of things
 
+foods = ["tacos", "apple", "ice cream"]
+p foods
+
+we_have_to_go_back = [4,8,15,16,23,42]
+p we_have_to_go_back
+
+mixed_array = ["tacos", 3, true]
+p mixed_array
+
+shopping_list = [ ["diapers", "baby wipes", "beer"], ["coffee", "more food"]]
+p shopping_list
+
 # Accessing the array
+puts foods[0]
+puts foods[1]
+puts foods[2]
+p foods[3]
+puts foods[-1]
+puts shopping_list[1][0]
 
 # Add to the array
+
+foods.push("salad")
+p foods
+
+# .append can also be used to add to arrays like push 
+# << also does this 
+
+
 
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -4,6 +4,27 @@
 
 # Hashes are lists of *key-value pairs*
 
+profile = {
+    "name" => "Ben", 
+    "location" => {"city" => "chicago", "state" => "IL"}, 
+    "timeline" => [
+        {"status" => "teaching still...", "occured_at"=> "8:30pm"},
+        {"status" => "driving home...", "occured_at"=> "9:30pm"}
+    ]
+}
+
+
 # Accessing data from the hash
 
+puts profile["name"]
+puts profile["status"]
+
+
+
+
 # More Complex Hashes
+puts profile["location"]["city"]
+#profile["status"] = "still teaching..."
+profile["phone"] = "123-456-7890"
+
+puts profile["timeline"][-1]["status"]
